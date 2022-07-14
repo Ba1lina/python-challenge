b7f41811799699e3d41b84c9de27f383d65158b4
Ba1lina <105493270+Ba1lina@users.noreply.github.com>
2022-07-14 23:04:43 +0800

    Create main.py

---
+++ PyPoll/main.py
@@ -0,0 +1,14 @@
+#the main
+
+from importlib.machinery import WindowsRegistryFinder
+from itertools import count
+from multiprocessing.sharedctypes import Value
+from struct import calcsize
+
+
+#total votes = count
+
+#sum of each person = count each unique Value
+#calcsize
+
+#WindowsRegistryFinder
